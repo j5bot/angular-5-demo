3942c189e897c0cff508ff0f5983cd7065e79ae2
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2018-05-01 12:18:10 -0400

    issue #13: split app state in reducer
    Split app state in reducer to allow for easy decoration of AppComponent with properties that access the presentation state (e.g. title, message, etc.)

--- src/app/reducers/app.ts
+++ src/app/reducers/app.ts
@@ -6,20 +6,31 @@ export interface State {
   message: String;
   showButtonText: String;
   showFeedback: boolean;
-  feedback: any;
-  error: any;
+  feedback: Object;
+  submission: Object;
+  error: boolean;
   errorMessage: String;
 }
 
-export const defaultState: State = {
-  title: 'What Do You Think About Us?',
-  message: `We're interested in learning your thoughts about our many products and services`,
-  showButtonText: 'Feedback',
-  showFeedback: false,
-  feedback: null,
-  error: null,
-  errorMessage: ''
+export const defaultPresentationState = {
+  title: <String> 'What Do You Think About Us?',
+  message: <String> `We're interested in learning your thoughts about our many products and services`,
+  showButtonText: <String> 'Feedback',
+  showFeedback: <boolean> false,
 };
+
+export const defaultApplicationState = {
+  feedback: <Object> null,
+  submission: <Object> null,
+  error: <boolean> null,
+  errorMessage: <String> ''
+};
+
+export const defaultState: State = Object.assign(
+  {},
+  defaultApplicationState,
+  defaultPresentationState
+);
 
 export function reducer ( state = defaultState, action: AppActions ): State {
 
