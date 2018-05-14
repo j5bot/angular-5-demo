92cc47ef08fe7edf8c2ae5f7a44c58ec50c6e8a6
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2018-05-14 19:03:44 -0400

    issue #26: enter-feedback-component tests pass

--- src/app/components/enter-feedback/enter-feedback.component.spec.ts
+++ src/app/components/enter-feedback/enter-feedback.component.spec.ts
@@ -1,20 +1,15 @@
 import {
-  async, ComponentFixture, ComponentFixtureAutoDetect, TestBed
+  ComponentFixture,
+  TestBed
 } from '@angular/core/testing';
 import {
   Input, Output,
   DebugElement
 } from '@angular/core';
-import {By} from "@angular/platform-browser";
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { marbles } from 'rxjs-marbles/jest';
 
-import { StoreModule, Store } from '@ngrx/store';
-<<<<<<< Updated upstream
-import { reducers, State } from '../../reducers';
-import { Observable } from 'rxjs/observable';
-=======
 import { Observable } from 'rxjs/Observable';
 import {
   AbstractControl,
@@ -23,21 +18,12 @@ import {
   ReactiveFormsModule
 } from '@angular/forms';
 
->>>>>>> Stashed changes
 import { MaterialModule } from '../../../modules/material-module';
-
-import { testObservable } from '../../../../modules/test-utilities';
-import { reducers, State } from '../../reducers';
-
 import { EnterFeedbackComponent } from './enter-feedback.component';
-
-// import * as FeedbackActions from '../../actions/feedback';
-// import * as selectors from '../../selectors/selectors';
 
 describe('EnterFeedbackComponent', () => {
   let fixture: ComponentFixture<EnterFeedbackComponent>;
   let instance: EnterFeedbackComponent;
-  let debugElement: DebugElement;
 
   const EMPTY_FORM = {
     name: '',
@@ -66,10 +52,10 @@ describe('EnterFeedbackComponent', () => {
   const ERRORS = {
     phone: '555-1212 something not so good',
     email: 'notanemail.address',
-    feedback: 42
+    feedback: null
   };
 
-  beforeEach( async(() => {
+  beforeEach( () => {
     TestBed.configureTestingModule({
       imports: [
         MaterialModule,
@@ -78,17 +64,11 @@ describe('EnterFeedbackComponent', () => {
       ],
       declarations: [
         EnterFeedbackComponent
-      ],
-      providers: [
-        { provide: ComponentFixtureAutoDetect, value: true }
       ]
     }).compileComponents();
-  }));
 
-  beforeEach( () => {
     fixture = TestBed.createComponent(EnterFeedbackComponent);
     instance = fixture.componentInstance;
-    debugElement = fixture.debugElement;
     fixture.detectChanges();
   });
 
@@ -112,299 +92,93 @@ describe('EnterFeedbackComponent', () => {
   * to validate the rendered output and verify the component's output
   * against changes in state.
   */
-  it('should match previous snapshot', async(() => {
+  it('should match previous snapshot', () => {
     expect(fixture).toMatchSnapshot();
-  }));
+  });
 
   describe('should emit onchange events when values change', () => {
 
-    // let prop;
-    // Object.keys(CHANGES).forEach(
-    //   function (prop) {
-    it(`should emit a name change event when setting the name`,
-    async(() => {
-      const prop = 'name';
-      const control: AbstractControl = instance.feedbackForm.controls[ prop ];
-      spyOn(instance, 'changeValue');
-      // instance.feedbackForm.setValue( { [prop]: CHANGES[ prop ] } );
-      console.log(`control ${ prop }
-      errors: ${ JSON.stringify(control.errors) }
-      valid: ${ control.valid }
-      value: ${ control.value }
-      status: ${ control.status }
-      pristine: ${ control.pristine }
-      untouched: ${ control.untouched }`);
+    Object.keys(CHANGES).forEach(
+      (prop) => {
+        it(`should emit a ${ prop } change event when setting the ${ prop }`,
+          () => {
+            const control: AbstractControl = instance.feedbackForm.controls[ prop ];
+            spyOn(instance, 'changeValue');
 
-      control.setValue(CHANGES[ prop ]);
-      instance.changeValue( prop, CHANGES[ prop ] );
+            //             console.log(`control ${ prop }
+            // errors: ${ JSON.stringify(control.errors) }
+            // valid: ${ control.valid }
+            // value: ${ control.value }
+            // status: ${ control.status }
+            // pristine: ${ control.pristine }
+            // untouched: ${ control.untouched }`);
 
-      fixture.detectChanges();
-      fixture.whenStable().then( () => {
+            control.setValue(CHANGES[ prop ]);
 
-        const compare: AbstractControl = instance.feedbackForm.controls[ prop ];
-        fixture.whenStable();
-
-        expect(instance.changeValue).toHaveBeenCalledWith(prop, CHANGES[ prop ]);
-        const errors = compare.errors || {};
-        console.log(`compare ${ prop }
-errors: ${ JSON.stringify(compare.errors) }
-valid: ${ compare.valid }
-value: ${ compare.value }
-status: ${ compare.status }
-pristine: ${ compare.pristine }
-untouched: ${ compare.untouched }`);
-          //  expect(control.valid).toBeTrue();
-        });
-      }
-    ));
-    //   }
-    // );
-
-
-    // Object.keys(CHANGES).forEach(
-    //   function (prop) {
-    it(`should emit a name change event when setting the name`,
-    async( () => {
-      const prop = 'phone';
-      const control: AbstractControl = instance.feedbackForm.controls[ prop ];
-      spyOn(instance, 'changeValue');
-      // instance.feedbackForm.setValue( { [prop]: CHANGES[ prop ] } );
-      console.log(`control ${ prop }
-errors: ${ JSON.stringify(control.errors) }
-valid: ${ control.valid }
-value: ${ control.value }
-status: ${ control.status }
-pristine: ${ control.pristine }
-untouched: ${ control.untouched }`);
-
-      control.setValue(CHANGES[ prop ]);
-      instance.changeValue( prop, CHANGES[ prop ] );
-
-      fixture.detectChanges();
-      fixture.whenStable().then( () => {
-        fixture.detectChanges();
-
-        expect(instance.changeValue).toHaveBeenCalledWith(prop, CHANGES[ prop ]);
-        const errors = control.errors || {};
-        console.log(`control ${ prop }
-errors: ${ JSON.stringify(control.errors) }
-valid: ${ control.valid }
-value: ${ control.value }
-status: ${ control.status }
-pristine: ${ control.pristine }
-untouched: ${ control.untouched }`);
-          //  expect(control.valid).toBeTrue();
-        });
-      }
-    ));
-
-
-
-    // Object.keys(CHANGES).forEach(
-    //   function (prop) {
-    it(`should emit a email change event when setting the email`,
-    async( () => {
-      const prop = 'email';
-      const control: AbstractControl = instance.feedbackForm.controls[ prop ];
-      spyOn(instance, 'changeValue');
-      // instance.feedbackForm.setValue( { [prop]: CHANGES[ prop ] } );
-      console.log(`control ${ prop }
-      errors: ${ JSON.stringify(control.errors) }
-      valid: ${ control.valid }
-      value: ${ control.value }
-      status: ${ control.status }
-      pristine: ${ control.pristine }
-      untouched: ${ control.untouched }`);
-
-      control.setValue(CHANGES[ prop ]);
-      instance.changeValue( prop, CHANGES[ prop ] );
-
-      fixture.detectChanges();
-      fixture.whenStable().then( () => {
-        fixture.detectChanges();
-
-        expect(instance.changeValue).toHaveBeenCalledWith(prop, CHANGES[ prop ]);
-        const errors = control.errors || {};
-        console.log(`control ${ prop }
-          errors: ${ JSON.stringify(control.errors) }
-          valid: ${ control.valid }
-          value: ${ control.value }
-          status: ${ control.status }
-          pristine: ${ control.pristine }
-          untouched: ${ control.untouched }`);
-          //  expect(control.valid).toBeTrue();
-        });
-      }
-    ));
-
-
-
-    // Object.keys(CHANGES).forEach(
-    //   function (prop) {
-    it(`should emit a feedback change event when setting the feedback`,
-    async( () => {
-      const prop = 'feedback';
-      const control: AbstractControl = instance.feedbackForm.controls[ prop ];
-      spyOn(instance, 'changeValue');
-      // instance.feedbackForm.setValue( { [prop]: CHANGES[ prop ] } );
-      console.log(`control ${ prop }
-      errors: ${ JSON.stringify(control.errors) }
-      valid: ${ control.valid }
-      value: ${ control.value }
-      status: ${ control.status }
-      pristine: ${ control.pristine }
-      untouched: ${ control.untouched }`);
-
-      control.setValue(CHANGES[ prop ]);
-      instance.changeValue( prop, CHANGES[ prop ] );
-
-      fixture.detectChanges();
-      fixture.whenStable().then( () => {
-        fixture.detectChanges();
-
-        expect(instance.changeValue).toHaveBeenCalledWith(prop, CHANGES[ prop ]);
-        const errors = control.errors || {};
-        console.log(`control ${ prop }
-          validators: ${ control.validators }
-          errors: ${ JSON.stringify(control.errors) }
-          valid: ${ control.valid }
-          value: ${ control.value }
-          status: ${ control.status }
-          pristine: ${ control.pristine }
-          untouched: ${ control.untouched }`);
-          //  expect(control.valid).toBeTrue();
-        });
-      }
-    ));
-
-
-
-    // Object.keys(ERRORS).forEach(
-    //   function (prop) {
-    it(`should have a phone error property after setting the phone`,
-    async( () => {
-      const prop = 'phone';
-      const control: AbstractControl = instance.feedbackForm.controls[ prop ];
-      spyOn(instance, 'changeValue');
-      console.log(`control ${ prop }
-        errors: ${ JSON.stringify(control.errors) }
-        valid: ${ control.valid }
-        value: ${ control.value }
-        status: ${ control.status }
-        pristine: ${ control.pristine }
-        untouched: ${ control.untouched }`);
-
-        control.setValue(ERRORS[ prop ]);
-        // instance.feedbackForm.setValue( { [prop]: ERRORS[ prop ] } );
-        instance.changeValue( prop, ERRORS[ prop ] );
-
-        fixture.detectChanges();
-        fixture.whenStable().then( () => {
-          fixture.detectChanges();
-
-          expect(instance.changeValue).toHaveBeenCalledWith(prop, ERRORS[ prop ]);
-          const errors = control.errors;
-          console.log(`control ${ prop }
-            validators: ${ control.validators }
-            errors: ${ JSON.stringify(control.errors) }
-            valid: ${ control.valid }
-            value: ${ control.value }
-            status: ${ control.status }
-            pristine: ${ control.pristine }
-            untouched: ${ control.untouched }`);
-            // expect(errors).toBeObject();
-          });
-        }
-      ));
-      //   }
-      // );
-
-
-      // Object.keys(ERRORS).forEach(
-      //   function (prop) {
-      it(`should have a email error property after setting the email`,
-      async( () => {
-        const prop = 'email';
-        const control: AbstractControl = instance.feedbackForm.controls[ prop ];
-        spyOn(instance, 'changeValue');
-        console.log(`control ${ prop }
-          errors: ${ JSON.stringify(control.errors) }
-          valid: ${ control.valid }
-          value: ${ control.value }
-          status: ${ control.status }
-          pristine: ${ control.pristine }
-          untouched: ${ control.untouched }`);
-
-          control.setValue(ERRORS[ prop ]);
-          // instance.feedbackForm.setValue( { [prop]: ERRORS[ prop ] } );
-          instance.changeValue( prop, ERRORS[ prop ] );
-
-          fixture.detectChanges();
-          fixture.whenStable().then( () => {
             fixture.detectChanges();
 
-            const compare: AbstractControl = instance.feedbackForm.controls[ prop ];
+            expect(instance.changeValue).toHaveBeenCalledWith(prop, CHANGES[ prop ]);
+            const errors = control.errors || {};
 
-            expect(instance.changeValue).toHaveBeenCalledWith(prop, ERRORS[ prop ]);
-            const errors = compare.errors;
-            console.log(`compare ${ prop }
-              errors: ${ JSON.stringify(compare.errors) }
-              valid: ${ compare.valid }
-              value: ${ compare.value }
-              status: ${ compare.status }
-              pristine: ${ compare.pristine }
-              untouched: ${ compare.untouched }`);
-              // expect(errors).toBeObject();
-            });
+            //             console.log(`compare ${ prop }
+            // errors: ${ JSON.stringify(control.errors) }
+            // valid: ${ control.valid }
+            // value: ${ control.value }
+            // status: ${ control.status }
+            // pristine: ${ control.pristine }
+            // untouched: ${ control.untouched }`);
+
+            expect(control.valid).toBeTrue();
+            expect(control.errors).toBeNull();
           }
-        ));
+        );
+      }
+    );
 
-        // Object.keys(ERRORS).forEach(
-        //   function (prop) {
-        it(`should have a feedback error property after setting the feedback`,
-        async( () => {
-          const prop = 'feedback';
-          const control: AbstractControl = instance.feedbackForm.controls[ prop ];
-          spyOn(instance, 'changeValue');
-          console.log(`control ${ prop }
-            errors: ${ JSON.stringify(control.errors) }
-            valid: ${ control.valid }
-            value: ${ control.value }
-            status: ${ control.status }
-            pristine: ${ control.pristine }
-            untouched: ${ control.untouched }`);
+    Object.keys(ERRORS).forEach(
+      (prop) => {
+        it(`should have a ${ prop } error property after setting the ${ prop }`,
+          () => {
+            const control: AbstractControl = instance.feedbackForm.controls[ prop ];
+
+            spyOn(instance, 'changeValue');
+
+            //             console.log(`control ${ prop }
+            // errors: ${ JSON.stringify(control.errors) }
+            // valid: ${ control.valid }
+            // value: ${ control.value }
+            // status: ${ control.status }
+            // pristine: ${ control.pristine }
+            // untouched: ${ control.untouched }`);
 
             control.setValue(ERRORS[ prop ]);
-            // instance.feedbackForm.setValue( { [prop]: ERRORS[ prop ] } );
-            instance.changeValue( prop, ERRORS[ prop ] );
 
             fixture.detectChanges();
-            fixture.whenStable().then( () => {
-              fixture.detectChanges();
 
-              const compare: AbstractControl = instance.feedbackForm.controls[ prop ];
+            expect(instance.changeValue).toHaveBeenCalledWith(prop, ERRORS[ prop ]);
+            const errors = control.errors;
 
-              expect(instance.changeValue).toHaveBeenCalledWith(prop, ERRORS[ prop ]);
-              const errors = compare.errors;
-              console.log(`compare ${ prop }
-                validators: ${ compare.validators }
-                errors: ${ JSON.stringify(compare.errors) }
-                valid: ${ compare.valid }
-                value: ${ compare.value }
-                status: ${ compare.status }
-                pristine: ${ compare.pristine }
-                untouched: ${ compare.untouched }`);
-                // expect(errors).toBeObject();
-              });
-            }
-          ));
+            //             console.log(`control ${ prop }
+            // errors: ${ JSON.stringify(control.errors) }
+            // valid: ${ control.valid }
+            // value: ${ control.value }
+            // status: ${ control.status }
+            // pristine: ${ control.pristine }
+            // untouched: ${ control.untouched }`);
 
-          // it('should emit a name change event when setting the name', () => {
-          //   spyOn(instance, 'changeValue');
-          //   instance.changeValue('name', CHANGES.name);
-          //   expect(instance.changeValue).toHaveBeenCalledWith('name', CHANGES.name);
-          // });
+            expect(errors).toBeObject();
+            expect(control.valid).toBeFalse();
+          });
+      }
+    );
+  });
 
-        });
+    // it('should emit a name change event when setting the name', () => {
+    //   spyOn(instance, 'changeValue');
+    //   instance.changeValue('name', CHANGES.name);
+    //   expect(instance.changeValue).toHaveBeenCalledWith('name', CHANGES.name);
+    // });
 
-      });
+  });
+
+});
