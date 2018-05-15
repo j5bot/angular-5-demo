f3257a56ea7c4b7bf0d1595dc1d8c30d95bde7b7
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2018-05-15 12:42:17 -0400

    issue #29: feedback-component tests
    first working tests for feedback-component

--- src/app/containers/feedback/feedback.component.spec.ts
+++ src/app/containers/feedback/feedback.component.spec.ts
@@ -1,227 +1,132 @@
 import {
-  Inject,
-  NgModule,
-  Component,
-  Directive,
-  Injector,
-  OverlayContainer,
-  ViewChild,
-  ViewContainerRef
+  NgModule
 } from '@angular/core';
-import { inject, async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatDialogModule } from '@angular/material';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { StoreModule, Store } from '@ngrx/store';
 
 import {
-  MatDialog
+  TestBed
+} from '@angular/core/testing';
+
+import {Overlay, OverlayContainer, OverlayModule} from '@angular/cdk/overlay';
+import {
+  MatIcon,
+  MatFormField,
+  MatError,
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatDialogModule
 } from '@angular/material';
 
-@Component({
-  template: `
-  <h1>This is in a dialog</h1>
-  `
-})
-class TestInDialogComponent {}
+import {
+  AbstractControl,
+  FormGroup,
+  ReactiveFormsModule
+} from '@angular/forms';
 
-@Directive({selector: 'dir-with-view-container'})
-class DirectiveWithViewContainer {
-  constructor(public viewContainerRef: ViewContainerRef) { }
-}
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-@Component({
-  selector: 'arbitrary-component',
-  template: `<dir-with-view-container></dir-with-view-container>`,
-})
-class ComponentWithChildViewContainer {
-  @ViewChild(DirectiveWithViewContainer) childWithViewContainer: DirectiveWithViewContainer;
+import { marbles } from 'rxjs-marbles/jest';
+import { testObservable } from '../../../../modules/test-utilities';
 
-  get childViewContainer() {
-    return this.childWithViewContainer.viewContainerRef;
-  }
-}
+import * as fromFeedback from '../../reducers/feedback';
+import { MaterialModule } from '../../../modules/material-module';
 
-@Directive({selector: 'dir-with-view-container'})
-class DirectiveWithViewContainer {
-  constructor(public viewContainerRef: ViewContainerRef) { }
-}
+import { FeedbackComponent } from './feedback.component';
+import { EnterFeedbackComponent } from '../../components/enter-feedback/enter-feedback.component';
+import { SubmitFeedbackComponent } from '../../components/submit-feedback/submit-feedback.component';
 
-@Component({
-  selector: 'arbitrary-component',
-  template: `<dir-with-view-container></dir-with-view-container>`,
-})
-class ComponentWithChildViewContainer {
-  @ViewChild(DirectiveWithViewContainer) childWithViewContainer: DirectiveWithViewContainer;
-
-  get childViewContainer() {
-    return this.childWithViewContainer.viewContainerRef;
-  }
-}
-
-describe('FeedbackComponent', () => {
-
-  let dialog: MatDialog;
-  let overlayContainerElement: HTMLElement;
-
-  let testViewContainerRef: ViewContainerRef;
-  let viewContainerFixture: ComponentFixture<ComponentWithChildViewContainer>;
-
-  beforeEach( async(() => {
-
-    TestBed.configureTestingModule({
-      imports: [
-        MatDialogModule,
-        DialogTestModule
-      ],
-      providers: [
-        { provide: OverlayContainer, useFactory: () => {
-          overlayContainerElement = document.createElement('div');
-          return { getContainerElement: () => overlayContainerElement };
-        }}
-      ]
-    });
-
-    TestBed.compileComponents();
-
-  }));
-
-  beforeEach(inject([MatDialog]), (d: MatDialog) => {
-    dialog = d;
-  });
-
-  beforeEach(() => {
-   viewContainerFixture = TestBed.createComponent(ComponentWithChildViewContainer);
-
-   viewContainerFixture.detectChanges();
-   testViewContainerRef = viewContainerFixture.componentInstance.childViewContainer;
- });
-
- it('open dialog', () => {
-   // let dialogRef = dialog.open( TestInDialogComponent, {
-   //   viewContainerRef: testViewContainerRef
-   // });
-   //
-   // expect(dialogRef).toBeTruthy();
- });
-
-});
-
-const TEST_DIRECTIVES = {
-  TestInDialogComponent
-};
+const TEST_DIRECTIVES = [
+  MatError,
+  FeedbackComponent,
+  EnterFeedbackComponent,
+  SubmitFeedbackComponent
+];
 
 @NgModule({
   imports: [
+    OverlayModule,
     MatDialogModule,
-    NoopAnimationsModule
+    MaterialModule,
+    NoopAnimationsModule,
+    ReactiveFormsModule
   ],
-  exports: [
-    ComponentWithChildViewContainer,
-    DirectiveWithViewContainer,
-    TestInDialogComponent
-  ],
-  declarations: [
-    ComponentWithChildViewContainer,
-    DirectiveWithViewContainer,
-    TestInDialogComponent
-  ],
+  exports: TEST_DIRECTIVES,
+  declarations: TEST_DIRECTIVES,
   entryComponents: [
-    ComponentWithChildViewContainer,
-    TestInDialogComponent
+    FeedbackComponent,
+    EnterFeedbackComponent
+  ],
+  providers: [
+    MaterialModule
   ]
 })
 class DialogTestModule { }
 
-// import { Component } from '@angular/core';
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { StoreModule, Store, combineReducers } from '@ngrx/store';
-// // import { MatDialogModule } from '../../../modules/material-module';
-// // import {
-// //   MatIcon,
-// //   MatDialogContent, MatDialogTitle, MatDialogActions,
-// //   MatButton, MatFormField, MatRipple } from '@angular/material';
-// import {
-//   MatIcon,
-//   MatDialog,
-//   MatDialogRef,
-//   MAT_DIALOG_DATA,
-//   MatDialogContent,
-//   MatDialogActions,
-//   MatFormField,
-//   MatRipple
-// } from '@angular/material';
-// import {
-//   MatInput
-// } from '@angular/material/input';
-//
-// import { FeedbackComponent } from './feedback.component';
-// import { EnterFeedbackComponent } from '../../components/enter-feedback/enter-feedback.component';
-// import { SubmitFeedbackComponent } from '../submit-feedback/submit-feedback.component';
-//
-// import * as feedbackActions from '../../actions/feedback';
-// import * as fromFeedback from '../../reducers/feedback';
-//
-// // import { MatDialog } from '@angular/material';
-// @Component({
-//   selector: 'app-test-host',
-//   template: `
-//   <h1>Test Host</h1>
-//   `
-// })
-// export class TestHostComponent {
-//   private dialog: MatDialog;
-//
-//   constructor () {
-//     // const dialogRef: MatDialogRef = this.dialog.open(
-//     //   FeedbackComponent, {
-//     //     width: '600px',
-//     //     height: '600px'
-//     //   }
-//     // );
-//   }
-// }
-//
-// describe('FeedbackComponent', () => {
-//   // let fixture: ComponentFixture<FeedbackComponent>;
-//   let fixture: ComponentFixture<TestHostComponent>;
-//   let store: Store<fromFeedback.State>;
-//   // let instance: FeedbackComponent;
-//   let instance = TestHostComponent;
-//   let dispatch: any;
-//
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       imports: [
-//         StoreModule.forRoot({
-//           feedback: combineReducers(fromFeedback.reducer)
-//         }),
-//         // MaterialModule
-//       ],
-//       declarations: [
-//         TestHostComponent,
-//         FeedbackComponent,
-//         EnterFeedbackComponent,
-//         SubmitFeedbackComponent,
-//         MatIcon,
-//         MatDialog,
-//         MatDialogContent,
-//         // MatDialogTitle,
-//         MatDialogActions,
-//         // MatButton,
-//         MatInput,
-//         MatFormField,
-//         MatRipple
-//       ]
-//     });
-//
-//     // fixture = TestBed.createComponent(FeedbackComponent);
-//     fixture = TestBed.createComponent(TestHostComponent);
-//
-//     store = TestBed.get(Store);
-//     instance = fixture.componentInstance;
-//
-//     dispatch = spyOn(store, 'dispatch');
-//   });
-//
+describe('FeedbackComponent', () => {
+  let dialog: MatDialog;
+  let dialogRef: MatDialogRef<FeedbackComponent>;
+  let store: Store<fromFeedback.State>;
+  let overlayContainer: OverlayContainer;
+  let overlayContainerElement: HTMLElement;
+  let overlay: Overlay;
+  let dispatch: any;
+
+  beforeAll( () => {
+    TestBed.configureTestingModule({
+      imports: [
+        DialogTestModule,
+        StoreModule.forRoot( fromFeedback.reducer )
+      ],
+      providers: [
+          { provide: OverlayContainer, useFactory: () => {
+            overlayContainerElement = document.createElement('div');
+            return { getContainerElement: () => overlayContainerElement };
+          }}
+        ]
+      }).compileComponents();
+
+    dialog = TestBed.get(MatDialog);
+    store = TestBed.get(Store);
+    overlay = TestBed.get(Overlay);
+    overlayContainer = TestBed.get(OverlayContainer);
+
+  });
+
+  beforeEach( () => {
+    dispatch = spyOn(store, 'dispatch');
+  });
+
+
+  it('should open', () => {
+
+    dialogRef = dialog.open(FeedbackComponent, {
+      width: '600px',
+      height: '600px'
+    });
+
+  });
+
+  it('should render', () => {
+    expect(dialogRef).toBeTruthy();
+  });
+
+  it('should render (overlay)', () => {
+    expect(overlay).toBeTruthy();
+  });
+
+  it('should render (overlay container)', () => {
+    expect(overlayContainer).toBeTruthy();
+  });
+
+  it('should match snapshot (overlay container element)', () => {
+    expect(overlayContainerElement).toMatchSnapshot();
+  });
+
+});
+
+
 //   /**
 //    * Container components are used as integration points for connecting
 //    * the store to presentational components and dispatching
