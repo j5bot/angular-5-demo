4449eae92a246ebb72c626175bbb57dcb884a44e
Jonathan 'J5' Cook <jonathan.j5.cook@gmail.com>
2018-05-15 19:18:47 -0400

    issue #29: feedback component unit tests

--- src/app/containers/feedback/feedback.component.spec.ts
+++ src/app/containers/feedback/feedback.component.spec.ts
@@ -2,6 +2,7 @@ import {
   NgModule
 } from '@angular/core';
 
+import { Observable } from 'rxjs/Observable';
 import { StoreModule, Store } from '@ngrx/store';
 
 import {
@@ -30,12 +31,15 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { marbles } from 'rxjs-marbles/jest';
 import { testObservable } from '../../../../modules/test-utilities';
 
+import * as FeedbackActions from  '../../actions/feedback';
 import * as fromFeedback from '../../reducers/feedback';
 import { MaterialModule } from '../../../modules/material-module';
 
 import { FeedbackComponent } from './feedback.component';
 import { EnterFeedbackComponent } from '../../components/enter-feedback/enter-feedback.component';
 import { SubmitFeedbackComponent } from '../../components/submit-feedback/submit-feedback.component';
+
+const defaultState = fromFeedback.defaultPresentationState;
 
 const TEST_DIRECTIVES = [
   MatError,
@@ -71,6 +75,7 @@ describe('FeedbackComponent', () => {
   let overlayContainer: OverlayContainer;
   let overlayContainerElement: HTMLElement;
   let overlay: Overlay;
+  let instance: FeedbackComponent;
   let dispatch: any;
 
   beforeAll( () => {
@@ -92,36 +97,155 @@ describe('FeedbackComponent', () => {
     overlay = TestBed.get(Overlay);
     overlayContainer = TestBed.get(OverlayContainer);
 
+    dialogRef = dialog.open(FeedbackComponent, {
+      width: '600px',
+      height: '600px'
+    });
+
+    instance = dialogRef.componentInstance;
+
   });
 
   beforeEach( () => {
     dispatch = spyOn(store, 'dispatch');
   });
 
-
-  it('should open', () => {
-
-    dialogRef = dialog.open(FeedbackComponent, {
-      width: '600px',
-      height: '600px'
-    });
-
+  it('should render dialog', () => {
+    expect(dialog).toBeTruthy();
+    // console.log(dialog);
   });
 
-  it('should render', () => {
+  it('should render dialogRef', () => {
     expect(dialogRef).toBeTruthy();
+    // console.log(dialogRef);
   });
 
   it('should render (overlay)', () => {
     expect(overlay).toBeTruthy();
   });
 
+  it('should render instance', () => {
+    expect(instance).toBeTruthy();
+    // console.log(instance);
+  });
+
   it('should render (overlay container)', () => {
     expect(overlayContainer).toBeTruthy();
+    // console.log(overlayContainer);
+    // console.log(overlayContainer.getContainerElement());
   });
 
   it('should match snapshot (overlay container element)', () => {
     expect(overlayContainerElement).toMatchSnapshot();
+  });
+
+  // it('should match snapshot (feedback component instance)', () => {
+  //   expect(instance).toMatchSnapshot();
+  // });
+
+  it('should have the default title', marbles((m) => {
+
+    testObservable({
+      fn: () => {},
+      marble: m,
+      expected: defaultState.title,
+      result: instance.title$
+    });
+
+  }));
+
+  it('should have the default submitted flag', marbles((m) => {
+
+    testObservable({
+      fn: () => {},
+      marble: m,
+      expected: defaultState.submitted,
+      result: instance.submitted$
+    });
+
+  }));
+
+  it('should have the default confirmed flag', marbles((m) => {
+
+    testObservable({
+      fn: () => {},
+      marble: m,
+      expected: defaultState.confirmed,
+      result: instance.confirmed$
+    });
+
+  }));
+
+  it('should have the default feedback object', marbles((m) => {
+
+    testObservable({
+      fn: () => {},
+      marble: m,
+      expected: defaultState.feedback,
+      result: instance.feedback$
+    });
+
+  }));
+
+  it('should have the default submission object', marbles((m) => {
+
+    testObservable({
+      fn: () => {},
+      marble: m,
+      expected: defaultState.submission,
+      result: instance.submission$
+    });
+
+  }));
+
+  it('should dispatch a change form action', () => {
+
+    const $event: any = { field: 'name', value: 'Not Another One' };
+    const action = new FeedbackActions.ChangeForm( $event );
+
+    instance.changeForm( $event );
+
+    expect( dispatch ).toHaveBeenCalledWith( action );
+  });
+
+  it('should dispatch a submit action', () => {
+
+    const $event: any = {};
+    const action = new FeedbackActions.SubmitFeedback( $event );
+
+    instance.submit( $event );
+
+    expect( dispatch ).toHaveBeenCalledWith( action );
+  });
+
+  it('should dispatch a confirm action', () => {
+
+    const $event: any = {};
+    const action = new FeedbackActions.ConfirmFeedback( $event );
+
+    instance.confirm( $event );
+
+    expect( dispatch ).toHaveBeenCalledWith( action );
+  });
+
+  it('should dispatch a change feedback action', () => {
+
+    const $event: any = {};
+    const action = new FeedbackActions.ChangeFeedback( $event );
+
+    instance.change( $event );
+
+    expect( dispatch ).toHaveBeenCalledWith( action );
+  });
+
+  it('should dispatch a cancel feedback action', () => {
+
+    const $event: any = {};
+    const action = new FeedbackActions.CancelFeedback( $event );
+
+    instance.cancel( $event );
+
+    expect( dispatch ).toHaveBeenCalledWith( action );
   });
 
 });
